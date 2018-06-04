0b6fe7308a6b30e04f65b154945edef9f9edf363
Ragadeepika23 <deepikavaratharaj23@gmail.com>
2018-06-04 15:02:17 +0530

    Created models for like and follower in Dwitter

--- dwitter/login/models.py
+++ dwitter/login/models.py
@@ -1,24 +1,40 @@
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Profile(models.Model):
+'''class Profile(models.Model):
     User_Name = models.CharField(unique = True , max_length = 200 )
     email = models.CharField(unique = True , max_length = 200)
     password = models.CharField(max_length = 8)
     age = models.IntegerField()
     gender = models.CharField(max_length=20)
     address = models.TextField()
-    birth_date = models.DateField()
+    birth_date = models.DateField()'''
 
 class Dweet(models.Model):
-    user_id = models.ForeignKey(Profile, on_delete=models.CASCADE )
-    Content = models.TextField()
+    user_id = models.ForeignKey(User,on_delete=models.CASCADE )
+    dweet = models.TextField()
     time = models.DateTimeField()
-#class Follower():
- #   user_id = models.ForeignKey(Profile, on_delete=models.CASCADE )
-  #  Follower_Id = models.ForeignKey(Profile, on_delete = models.CASCADE)
-   # No_of_followers = models.IntegerField()
 
+class Likes(models.Model):
+    Liker_id = models.ForeignKey(User,on_delete = models.CASCADE)
+    time = models.DateTimeField()
+
+class Follower(models.Model):
+    Fol_Id = models.ForeignKey(User,on_delete = models.CASCADE)
+    time = models.DateTimeField()
+    follower_no = models.ManyToManyField('self', related_name='follows', symmetrical=False)
+
+class Comment(models.Model):
+    user_id = models.ForeignKey(User,on_delete = models.CASCADE)
+    comment = models.TextField()
+    time = models.DateTimeField()
+
+'''class Chat(models.Model):
+    user_id = models.ForeignKey(User, on_delete = models.CASCADE)
+    Msg_sender = models.TextField()
+    Msg_receiver = models.TextField()
+    time = models.DateTimeField()
+    sent_time = models.DateTimeField(default = timezone.now)'''
 
